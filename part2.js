4e39d77d7f804c0b1c48ef8913934ca665942a07
johnOdia <63448302+johnOdia@users.noreply.github.com>
2020-08-20 18:20:16 -0700

    Update part2.js

--- part2.js
+++ part2.js
@@ -1,37 +1,29 @@
 //VOWEL COUNT
 const vowelCount = string => {
-    let arr = []
-    string = string.split("")
-    const str = Array.from(new Set(string))
-    str.forEach(val => {
-        if(val === 'a'  || val === 'e' || val === 'i' || val === 'o' || val === 'u' ) {
-            arr.push(val)
+    let stringArray = [...string]//changes the string into an array using the spread operator. Another way is using string.split("") method
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let obj = stringArray.reduce((acc, val) => {
+        if(vowels.indexOf(val) !== -1){ //checks to see if the val at current iteration count is in the vowels array. if it isn't it will return -1.
+            acc[val] = (acc[val] || 0) + 1; //sets the objects key and value.
         }
-    })
-    const myObj = arr.reduce((a, key) => Object.assign(a, { [key]: 0 }), {})
-    for(let i in myObj){
-       for(let j = 0; j < string.length; j++){
-           if (i === string[j]){
-               myObj[i] += 1
-           }
-       }
-    }
-    return myObj
+        return acc
+    },{})
+    return obj
 }
+console.log(vowelCount('incredible'))
 
-console.log(vowelCount('awesome'))
+
 
 //REMOVE VOWELS
 const removeVowels = str => {
-    let string = str.toLowerCase()
+    let string = [...str.toLowerCase()]// forgive me i love using spread operators!
+    let vowels = ['a', 'e', 'i', 'o', 'u']
     let output = ''
-    for (let i = 0; i < string.length; i++){
-        if(string[i] === 'a' || string[i] === 'o' || string[i] === 'u'  || string[i] === 'i' || string[i] === 'e' ){
-            continue
+    string.forEach(val => {
+        if(vowels.indexOf(val) === -1 ){ //checks to see if the val is in the vowels array. if it's not (returns -1) then it must be a consonant.
+            output += val //we then add the value to our output variable
         }
-        output += string[i]
-    }
+    })
     return output
 }
-
-console.log(removeVowels('love'))
+console.log(removeVowels('efosa'))
